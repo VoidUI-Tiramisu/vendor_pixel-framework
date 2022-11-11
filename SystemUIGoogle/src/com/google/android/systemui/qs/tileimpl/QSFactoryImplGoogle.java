453dc414b067a7590437aa991e88ca146e1087f7
johnmart19 <johnivan19999@gmail.com>
2022-11-11 17:20:07 +0200

    SystemUIGoogle: Add Auto Brightness QS Tile
    Change-Id: Ie9328d16e0b111f77595ce62b4c3df0152400f13

--- SystemUIGoogle/src/com/google/android/systemui/qs/tileimpl/QSFactoryImplGoogle.java
+++ SystemUIGoogle/src/com/google/android/systemui/qs/tileimpl/QSFactoryImplGoogle.java
@@ -68,6 +68,7 @@ import com.android.systemui.qs.tiles.ReadingModeTile;
 import com.android.systemui.qs.tiles.AntiFlickerTile;
 
 // Custom VoidUI Project
+import com.android.systemui.qs.tiles.AutoBrightnessTile;
 import com.android.systemui.qs.tiles.DataSwitchTile;
 
 import javax.inject.Inject;
@@ -127,7 +128,8 @@ public class QSFactoryImplGoogle extends QSFactoryImpl {
             Provider<LiveDisplayTile> liveDisplayTileProvider,
             Provider<ReadingModeTile> readingModeTileProvider,
             Provider<AntiFlickerTile> antiFlickerTileProvider,
-            Provider<DataSwitchTile> dataSwitchTileProvider) {
+            Provider<DataSwitchTile> dataSwitchTileProvider,
+            Provider<AutoBrightnessTile> autoBrightnessTileProvider) {
         super(qsHostLazy,
                 customTileBuilderProvider,
                 wifiTileProvider,
@@ -171,7 +173,8 @@ public class QSFactoryImplGoogle extends QSFactoryImpl {
                 liveDisplayTileProvider,
                 readingModeTileProvider,
                 antiFlickerTileProvider,
-                dataSwitchTileProvider);
+                dataSwitchTileProvider,
+                autoBrightnessTileProvider);
         mReverseChargingTileProvider = reverseChargingTileProvider;
         mBatterySaverTileGoogleProvider = batterySaverTileGoogleProvider;
     }
